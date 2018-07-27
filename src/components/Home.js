a782feeaccef59fab25b0d8bc569c512a4829b36
summerjacket <dxmcxd@gmail.com>
2018-07-26 23:08:34 -0700

    commit before eject

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>
+      Home
+    </h1>
+    <p>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </p>
+    <p>
+       Nunc ut dui nec sapien semper porta. Nulla facilisi.
+    </p>
+    <p>
+       Vivamus feugiat mollis sapien, et viverra justo tempor at.
+    </p>
+    <p>
+       Aliquam ac ultricies elit, a accumsan sapien.
+    </p>
+  </div>
+);
+
+export default Home;
