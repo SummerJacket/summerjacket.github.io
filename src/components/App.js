c1d68510837ab65e3c2d8d65b8d09cc16918bfae
summerjacket <dxmcxd@gmail.com>
2018-07-30 23:32:45 -0700

    sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,38 @@ import { PoseGroup } from 'react-pose';
 
 import Home from './Home';
 import About from './About';
+import Sidebar from './Sidebar';
 // import Wipe from './Wipe';
 
-const Container = styled.div``;
+const Container = styled.div`
+  color: rgba(255, 255, 255, 0.95);
+  background: #15303d;
+  min-height: 100vh;
+`;
+
+const Row = styled.div`
+  display: flex;
+`;
+
+const Column = styled.div`
+`;
 
 const App = () => (
   <Container>
     {/* <Wipe /> */}
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-    <PoseGroup animateOnMount preEnterPose="before">
-      <Route exact path="/" component={Home} key={1} />
-      <Route path="/about" component={About} key={2} />
-    </PoseGroup>
+    <Row>
+      <Column style={{ width: 100 }}>
+        <Sidebar />
+      </Column>
+      <Column style={{ padding: '8em' }}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <PoseGroup animateOnMount preEnterPose="before">
+          <Route exact path="/" component={Home} key={1} />
+          <Route path="/about" component={About} key={2} />
+        </PoseGroup>
+      </Column>
+    </Row>
   </Container>
 );
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  position: fixed;
+  width: inherit;
+  height: 100vh;
+  background: white;
+`;
+
+const Sidebar = () => (
+  <Wrapper />
+);
+
+export default Sidebar;
