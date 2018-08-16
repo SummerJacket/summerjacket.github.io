8ae175217ea1ffb1886b6f300714b8972c29597f
summerjacket <dxmcxd@gmail.com>
2018-08-15 22:27:46 -0700

    Contact form

--- src/Contact.js
+++ src/Contact.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import { Row, Col, Button, Form, FormGroup, Input } from 'reactstrap';
 
 import VerticalAligner from './VerticalAligner';
 
+const ContactForm = () => (
+  <Form action="">
+    <Row>
+      <Col>
+        <FormGroup>
+          <Input type="text" name="name" placeholder="Your Name" />
+        </FormGroup>
+      </Col>
+      <Col>
+        <FormGroup>
+          <Input type="email" name="email" placeholder="Email" />
+        </FormGroup>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <FormGroup>
+          <Input
+            type="textarea"
+            name="message"
+            placeholder="Your message..."
+            rows="8"
+          />
+        </FormGroup>
+      </Col>
+    </Row>
+    <Button outline color="primary">
+      Submit
+    </Button>
+  </Form>
+);
+
 const Contact = () => (
-  <VerticalAligner align="end">
-    <h1>Hello World!</h1>
+  <VerticalAligner align="center">
+    <h1>Let&apos;s talk</h1>
+    <ContactForm />
   </VerticalAligner>
 );
 
