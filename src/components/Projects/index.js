b150fb4eee4cb0109e935c3ba81fa13791c850c8
summerjacket <dxmcxd@gmail.com>
2018-08-24 17:53:31 -0700

    update project page

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import { Row, Col } from 'reactstrap';
 
-const Projects = () => <h1>Projects</h1>;
+import childrenPropTypes from '../../childrenPropTypes';
+
+const CustomCol = ({ children, ...rest }) => (
+  <Col sm={12} md={6} lg={4} {...rest}>
+    {children}
+  </Col>
+);
+
+const Projects = () => (
+  <Row>
+    <CustomCol>Test1</CustomCol>
+    <CustomCol>Test2</CustomCol>
+    <CustomCol>Test3</CustomCol>
+  </Row>
+);
+
+CustomCol.propTypes = {
+  children: childrenPropTypes,
+};
+
+CustomCol.defaultProps = {
+  children: '',
+};
 
 export default Projects;
