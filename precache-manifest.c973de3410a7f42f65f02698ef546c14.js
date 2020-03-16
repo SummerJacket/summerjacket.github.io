896e477d43206f4a268bb4e881208b20f1215bcc
jasonliang512 <jasonliang512@gmail.com>
2020-03-15 21:11:06 -0700

    Updates

---
+++ precache-manifest.c973de3410a7f42f65f02698ef546c14.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "d97fe3c6ef19dd34738d27373698466b",
+    "url": "/images/chat-app.png"
+  },
+  {
+    "revision": "27d9e392d9fe675b782e085818d78591",
+    "url": "/images/heroicons.png"
+  },
+  {
+    "revision": "c6b08746d95546c0e965a20a3564be64",
+    "url": "/images/temtem.png"
+  },
+  {
+    "revision": "7a6559fb798c5becf498d7910a5d0c94",
+    "url": "/images/wildermaze.png"
+  },
+  {
+    "revision": "93d4e319dc2b2b2e042b1ee1b9ec0291",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "76bc2df6371f0534ed82",
+    "url": "/static/css/main.3abfcd91.chunk.css"
+  },
+  {
+    "revision": "76bc2df6371f0534ed82",
+    "url": "/static/js/main.76bc2df6.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
