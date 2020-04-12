91d81aa9e88bea07522bf6b8f8a065826321f6cd
Jason Liang <jasonliang512@gmail.com>
2020-04-12 01:48:01 -0700

    Fix purgecss extractor (hopefully)

--- webpack.config.js
+++ webpack.config.js
@@ -53,8 +53,23 @@ module.exports = {
                   ? [
                       purgecss({
                         content: ["./public/index.html", "./src/**/*.svelte"],
-                        defaultExtractor: (content) =>
-                          content.match(/[\w-/:]+(?<!:)/g) || [],
+                        defaultExtractor: (content) => {
+                          const regExp = new RegExp(/[A-Za-z0-9-_:/]+/g);
+                          let matchedTokens = [];
+                          let match = regExp.exec(content);
+
+                          while (match) {
+                            if (match[0].startsWith('class:')) {
+                              matchedTokens.push(match[0].substring(6));
+                            } else {
+                              matchedTokens.push(match[0]);
+                            }
+
+                            match = regExp.exec(content);
+                          }
+
+                          return matchedTokens;
+                        }
                       }),
                     ]
                   : []),
